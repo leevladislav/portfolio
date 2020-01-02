bbe70957dad36008b2410996684445a4a56aa7de
leevladislav <vladislavlee.xx@gmail.com>
2020-01-02 09:12:56 +0200

    removed bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,20 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {RouterModule} from '@angular/router';
-
+import {TechnologySliderComponent} from './technology-slider/technology-slider.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    TechnologySliderComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    TechnologySliderComponent
   ]
 })
 export class SharedModule {
