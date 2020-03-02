26fde5fe7379d7123666405d30b5012f218e3981
vladislav_lee <vladislav.lee.bekey@gmail.com>
2020-03-02 10:00:12 +0200

    removed unused folder

--- src/assets/projects/untitled folder/axit/js/script.js
+++
@@ -1,7 +0,0 @@
-$('.tabs-nav-item').on('click', function() {
-	$('.tabs-nav-item').removeClass('active');
-	$(this).addClass('active');
-	var tabNumber = $(this).attr('data-tab-number');
-	$('.tabs-content-title').removeClass('active');
-	$('.tabs-content-title[data-tab-number="' + tabNumber + '"]').addClass('active');
-});
--- src/assets/projects/untitled folder/smak/js/tabs.js
+++
@@ -1,7 +0,0 @@
-$('.team-item').on('click', function() {
-	$('.team-item').removeClass('active');
-	$(this).addClass('active');
-	var tabNumber = $(this).attr('data-tab-number');
-	$('.content-item').removeClass('active');
-	$('.content-item[data-tab-number="' + tabNumber + '"]').addClass('active');
-});
