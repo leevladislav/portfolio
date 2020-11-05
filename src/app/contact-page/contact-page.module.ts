f24e866dc70d7b1e0638e9f31e303f91138f71f0
vladislav_lee <vladislav.lee.bekey@gmail.com>
2020-11-05 18:54:45 +0200

    added contact page

---
+++ src/app/contact-page/contact-page.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ContactPageComponent} from './contact-page.component';
+import {ContactPageRoutingModule} from './contact-page.routing.module';
+
+
+@NgModule({
+  declarations: [ContactPageComponent],
+  imports: [
+    CommonModule,
+    ContactPageRoutingModule
+  ]
+})
+export class ContactPageModule {
+}
